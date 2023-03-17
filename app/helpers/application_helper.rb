e9a6a7f9bde4c1f10c64ebaedf11345cfe80ee96
Rian Dos Santos <riannegreiros@gmail.com>
2023-03-17 17:04:36 -0300

    Helper for ChatGPT response

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def generate_response()
+    api_key = ENV.fetch("OPENAI_API_KEY")
+    engine = ENV.fetch("OPENAI_ENGINE")
+    prompt = "This is a test."
+
+    openai_client = OpenAI::Client.new(api_key: api_key, default_engine: engine)
+    response = openai_client.completions(prompt: prompt, max_tokens: 5000, engine: engine)
+    text = response.data["choices"][0]["text"]
+  end
 end
