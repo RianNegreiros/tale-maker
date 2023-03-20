7dc212697edc83fed42628da1da98c891ea22ca7
Rian Dos Santos <riannegreiros@gmail.com>
2023-03-20 17:24:04 -0300

    config prompt on env file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
+  api_key = ENV.fetch("OPENAI_API_KEY")
   def generate_response()
-    api_key = ENV.fetch("OPENAI_API_KEY")
     engine = ENV.fetch("OPENAI_ENGINE")
-    prompt = "This is a test."
+    prompt = ENV.fetch("OPENAI_PROMPT")
 
     openai_client = OpenAI::Client.new(access_token: api_key, default_engine: engine)
     response = openai_client.completions(prompt: prompt, max_tokens: 5000, engine: engine)
@@ -10,12 +10,12 @@ module ApplicationHelper
   end
 
   def generate_image()
-    api_key = ENV.fetch("OPENAI_API_KEY")
+    prompt = ENV.fetch("DALL_E_PROMPT")
     OpenAI.configure do |config|
       config.api_key = api_key
     end
     openai_client = OpenAI::Client.new(access_token: api_key)
-    response = openai_client.images.generate(prompt: "Random image for testing", size: "512x512")
+    response = openai_client.images.generate(prompt: prompt, size: "512x512")
     response.dig("data", 0, "url")
   end
 end
